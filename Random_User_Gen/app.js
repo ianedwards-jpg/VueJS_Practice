d2340b6ff642bc777ea95472b61f9f4d8b616e39
Ian Edwards <ian.edward@uconn.edu>
2021-08-09 00:03:58 -0400

    First Update
    Created Frontend to display example Users
    Created API call to call example users

---
+++ Random_User_Gen/app.js
@@ -0,0 +1,22 @@
+const app = Vue.createApp({
+    // template: '<h1>Hello {{firstName}}</h1>', //Uses Declarative Rendering- I.e calls an object by an identifer 
+    data() {
+        return {
+            firstName: 'John',
+            lastName: 'Doe',
+            email: 'john.doe@gmail.com',
+            gender: 'male',
+            picture: 'https://randomuser.me/api/portraits/men/10.jpg',
+        }
+    },
+    methods: {
+        async getUser() {
+            const res = await fetch('https://randomuser.me/api') //API Call to obtain random user data objects
+            const { results } = await res.json() //Destructure results object via .json() method
+            console.log(results)
+            // console.log(results[0].name.first) // Test API call to get random results for 1 user
+        },
+    },
+})
+
+app.mount('#app')
