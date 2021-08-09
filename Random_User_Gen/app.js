135cf0ad65fe53da0cf77f41b0dd8fab2d7b4d54
Ian Edwards <ian.edward@uconn.edu>
2021-08-09 00:47:06 -0400

    Update/Complete || Created New Project
    Updated and completed rando user generator
    
    Created Vue Crash 2021 Example Application

--- Random_User_Gen/app.js
+++ Random_User_Gen/app.js
@@ -14,9 +14,19 @@ const app = Vue.createApp({
             const res = await fetch('https://randomuser.me/api') //API Call to obtain random user data objects
             const { results } = await res.json() //Destructure results object via .json() method
             console.log(results)
+
+            this.firstName = results[0].name.first
+            this.lastName = results[0].name.last
+            this.email = results[0].email
+            this.gender = results[0].gender
+            this.picture = results[0].picture.large
+
             // console.log(results[0].name.first) // Test API call to get random results for 1 user
         },
     },
 })
 
 app.mount('#app')
+
+
+// https://codepen.io/bradtraversy/pen/LYbzJjK
